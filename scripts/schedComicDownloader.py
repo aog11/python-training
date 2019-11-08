3ec19f2dbe63b438ef176705120c2f48718183ab
aog11 <antonifco@gmail.com>
2019-11-08 08:59:04 -0400

    Extending the Scheduled Web Comic Downloader Project

--- scripts/schedComicDownloader.py
+++ scripts/schedComicDownloader.py
@@ -17,6 +17,10 @@ currDate = currDate.strftime('%d-%m-%Y')
 comicsDir = ''
 os.chdir(comicsDir)
 
+# Creating the folder for the comic's date
+os.makedirs(currDate, exist_ok=True)
+os.chdir(currDate)
+
 # Downloading the comic webpage
 comicUrl = 'http://theawkwardyeti.com/'
 res = requests.get(comicUrl)
@@ -27,11 +31,19 @@ nextComic = webData.select('.navi.navi-next.navi-void')
 
 # You're at the latest one
 if not nextComic == []:
-    # Downloading the image
+    # Downloading the cover image
     comic = webData.select('#comic img')
     res = requests.get(comic[0].get('src'))
     # Getting the extension of the image
     imgExtension = os.path.splitext(comic[0].get('src'))[1]
-    with open('theawkwardyeti_%s%s'%(currDate,imgExtension),'wb') as imageFile:
+    with open('cover_theawkwardyeti_%s%s'%(currDate,imgExtension),'wb') as imageFile:
         for chunk in res.iter_content(100000):
             imageFile.write(chunk)
+    # Downloading the rest of the comic
+    comics = webData.select('.entry p img')
+    for i in range(len(comics)):
+        res = requests.get(comics[i].get('src'))
+        comicName = os.path.basename(comics[i].get('src'))
+        with open(comicName,'wb') as ImageFile:
+            for chunk in res.iter_content(100000):
+                ImageFile.write(chunk)
